cc700ebd7de87b5ea6f7308b1f065cf92e07ab31
davidmerchan04 <100416357@alumnos.uc3m.es>
2020-03-24 20:58:45 +0100

    Data Table

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ import json
 
 import pandas as pd
 
-db_url = 'https://datos.alcobendas.org/dataset/9cc894a1-8cfb-4dfe-a29f-fb197aa03ae0/resource/eff1bb9c-110e-4962-8370-d78589f987c2/download/uso-de-autobuses.csv'
-db = pd.read_csv(db_url)
-
+df_url = 'https://datos.alcobendas.org/dataset/9cc894a1-8cfb-4dfe-a29f-fb197aa03ae0/resource/eff1bb9c-110e-4962-8370-d78589f987c2/download/uso-de-autobuses.csv'
+df = round(pd.read_csv(df_url),2)
 
 
 app = dash.Dash(__name__)
@@ -22,21 +21,29 @@ server = app.server
 
 app.layout = html.Div([
     html.H2('Public transport of Alcobendas'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
+    dash_table.DataTable(
+    id='table',
+    columns=[{"name":i,"id":i} for i in df.columns],
+    data=df.to_dict('records'),
+    style_cell={'textAlign': 'center'},
+    style_cell_conditional=[
+        {
+            'if':{'column_id':'Tipo de transporte'},
+            'textAlign':'left'
+        }],
+    filter_action="native",
+    sort_action="native",
+    sort_mode="multi",
+    column_selectable="single",
+    #row_selectable="multi",
+    #row_deletable=True,
+    #selected_columns=[],
+    #selected_rows=[],
+    page_action="native",
+    #page_current= 0,
+    #page_size= 10,   
+    )
 ])
-
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
-
-
-@app.callback(dash.dependencies.Output('display-value', 'children'), [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
